47129b3e05a9f28b7612119b1266a9f0697f0921
Barnabas Edubio <barny.edubio@gmail.com>
2017-11-02 22:47:40 +0100

    countdown works on a basic level, must still add pause/resume/reset and settings

--- script.js
+++ script.js
@@ -30,9 +30,11 @@ function notificationPermission() {
 function pomodoroMain() {
 
     // initialize necessary buttons
-    let descButton      = document.getElementsByClassName("page-description"),
+    let mainText        = document.getElementById("main_text"),
+        descButton      = document.getElementsByClassName("page-description"),
         letsGoButton    = document.getElementById("lets_go"),
-        workDescription = document.getElementById("work_description");
+        workDescription = document.getElementById("work_description"),
+        startButton     = document.getElementById("pause_resume");
 
     // remove "maximize work output text"
     $(descButton).animate({"opacity": "0"}, 200, function(){
@@ -55,13 +57,57 @@ function pomodoroMain() {
                         "bottom" : "3rem"
                     }).animate({"opacity": "1"}, 100, function () {
                         // display "get work done" above timer
-                        $(workDescription).animate({"opacity": "1"}, 900);
+                        $(workDescription).animate({"opacity": "1"}, 300);
                     });
                 });
             });
         });
     });
 
+    /*
+    let pomodoroArray = [1500, 300, 1500, 300, 1500, 300, 1500, 900]; // default pomodoro "hyperperiod" in seconds
+    let counter = 0;
+    while (counter > -1) {
+        let index = counter % pomodoroArray.length;
+        countDown(index);
+        counter++;
+    } */
 
+    startButton.addEventListener("click", function () {
+        renderTime(convertToMinutes(1500-1));
+        countDown(1500-2);
+    });
+}
 
+function renderTime(time) {
+    document.getElementById("main_text").textContent = time;
+    document.getElementsByTagName("title")[0].textContent = time;
+}
+
+// uses JS's setInterval function to imitate seconds going by
+function countDown(seconds) {
+    let counter = seconds;
+    let interval = setInterval(function() {
+        renderTime(convertToMinutes(counter));
+        if (counter-- === 0) clearInterval(interval);
+    }, 1000);
+}
+
+// receives a number as input and returns the corresponding minute
+function convertToMinutes(seconds) {
+    let minuteString = "";
+
+    let minuteVal = seconds / 60;
+
+    if (seconds % 60 === 0){
+        minuteString = minuteVal.toString() + ":00";
+    } else {
+        let minuteStringMinute = minuteVal.toString().split(".")[0];
+        let minuteStringSecond = minuteVal.toString().split(".")[1];
+        minuteStringSecond = Math.round((parseFloat(("0." + minuteStringSecond)) * 60)).toString();
+        if (minuteStringSecond.length === 1) minuteStringSecond = "0" + minuteStringSecond;
+
+        minuteString = minuteStringMinute + ":" + minuteStringSecond;
+    }
+    return minuteString;
 }
