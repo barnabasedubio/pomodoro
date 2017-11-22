f6fb92fcc2808dba26883436fc5493acba557452
Barnabas Edubio <barny.edubio@gmail.com>
2017-11-22 01:09:35 +0100

    changed path to notification sound

--- src/script.js
+++ src/script.js
@@ -45,7 +45,7 @@ document.addEventListener('DOMContentLoaded', function () {
         countdownInterval,
         isRunning = false;
 
-    let audio = new Audio("media/notification.mp3");
+    let audio = new Audio("../media/notification.mp3");
 
     // animations
     function renderPage() {
